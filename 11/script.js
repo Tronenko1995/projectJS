59466c60845d5a0613fcaabaf0e652e98052361e
Tronenko <tronenko.aleksandr@gmail.com>
2020-10-24 10:11:58 +0300

    11 задание, решение мастера

--- 11/script.js
+++ 11/script.js
@@ -1,18 +1,20 @@
 "use strict"
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрети?','');
-console.log (numberOfFilms);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрети?','');
 
-let personalMovieDB = {
+const personalMovieDB = {
     count :  numberOfFilms,
     movies : {},
     actors : {},
     genres : [],
     privat : false
 };
-console.log (personalMovieDB);
 
-let quest1 = prompt('Один из последних просмотренных фильмов','');
-let quest2 = prompt('На сколько оцените его?','');
-personalMovieDB.movies = {quest1:quest2};
+const a = prompt('Один из последних просмотренных фильмов',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов',''),
+      d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 console.log(personalMovieDB);
